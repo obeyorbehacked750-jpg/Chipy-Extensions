d2c2eea9f6594c0fe32cb434306b222612d0a9b6
obeyorbehacked750-jpg <obeyorbehacked750@gmail.com>
2026-08-11 15:34:54 +0300

    Update chipyuttils.js

--- extensions/chipyuttils.js
+++ extensions/chipyuttils.js
@@ -1,8 +1,6 @@
 (function (Scratch) {
   'use strict';
 
-  const iconURL = 'https://media.discordapp.net/attachments/1533845150764236911/1536422173592526918/5a058dfa-595f-4340-944d-ac43efa15aa0.jpg?ex=6a7b5827&is=6a7a06a7&hm=0312f61748be6ef7c73f5c4ba90e6330046a4d1a2caac26f8983bfa797f1f533&=&format=webp';
-
   class ChipyUttils {
     getInfo() {
       return {
@@ -10,8 +8,6 @@
         name: 'Chipy Uttils',
         color1: '#FF4444', 
         color2: '#CC0000',
-        blockIconURI: iconURL,
-        menuIconURI: iconURL,
         blocks: [
           {
             opcode: 'pointTowards',
@@ -38,7 +34,7 @@
               SPRITE: { type: Scratch.ArgumentType.STRING, menu: 'spriteMenu' }
             }
           },
-          "---", // Adds a nice dividing line in the block menu
+          "---",
           {
             opcode: 'isTouchingClone',
             blockType: Scratch.BlockType.BOOLEAN,
@@ -68,7 +64,6 @@
           }
         ],
         menus: {
-          // This creates the dropdown list so you don't have to type sprite names!
           spriteMenu: {
             acceptReporters: true,
             items: '_getSprites'
@@ -77,7 +72,6 @@
       };
     }
 
-    // Automatically grabs all Sprite names for the dropdown menu
     _getSprites() {
       const spriteNames = [];
       const targets = Scratch.vm.runtime.targets;
@@ -111,7 +105,6 @@
       return { clone: closest, distance: minDistance };
     }
 
-    // New helper: Finds the specific clone you are physically touching
     _getTouchedClone(util, spriteName) {
       let closestTouch = null;
       let minDistance = Infinity;
@@ -121,13 +114,11 @@
         if (!target.isOriginal && target.sprite.name === spriteName) {
           if (target.id === util.target.id) continue;
           
-          // Uses TurboWarp's pixel-perfect collision engine
           if (Scratch.vm.renderer.isTouchingDrawables(util.target.drawableID, target.drawableID)) {
             const dx = target.x - util.target.x;
             const dy = target.y - util.target.y;
             const distance = Math.sqrt((dx * dx) + (dy * dy));
 
-            // If touching multiple clones, interact with the one closest to the center
             if (distance < minDistance) {
               minDistance = distance;
               closestTouch = target;
@@ -181,7 +172,6 @@
     getTouchedCloneVar(args, util) {
       const touchedClone = this._getTouchedClone(util, args.SPRITE);
       if (touchedClone) {
-        // Look up the specific variable on that specific clone
         const variable = touchedClone.lookupVariableByNameAndType(args.VAR, '');
         if (variable) return variable.value;
       }
